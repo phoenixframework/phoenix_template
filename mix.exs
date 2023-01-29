b8316982e87af4712818d84fe36c2650b525d11b
José Valim <jose.valim@dashbit.co>
2023-01-29 17:50:46 +0100

    Release v1.0.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixTemplate.MixProject do
   use Mix.Project
 
-  @version "1.0.0"
+  @version "1.0.1"
   @source_url "https://github.com/phoenixframework/phoenix_template"
 
   def project do
