08877a4b14f6b63970bd7a9d12fe87e24f6112e3
Benjamin Milde <benni@kobrakai.de>
2025-03-14 16:19:18 +0100

    export locals without parens for embed_templates (#9)

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,5 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  export: [locals_without_parens: [embed_templates: 1, embed_templates: 2]]
 ]
